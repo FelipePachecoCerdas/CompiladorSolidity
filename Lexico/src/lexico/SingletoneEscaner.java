32e475a0df801656e07b1b41a31bab9e5fab84cd
FelipePacheco <felipepace09@gmail.com>
2020-05-30 13:39:56 -0600

    Interfaz Decente :D

--- Lexico/src/lexico/SingletoneEscaner.java
+++ Lexico/src/lexico/SingletoneEscaner.java
@@ -15,75 +15,78 @@ import java.util.Map;
  * @author usuario
  */
 class SingletoneEscaner {
-    private static SingletoneEscaner single_instance = null;
-    
-    
-    private SingletoneEscaner() 
-    { 
-    } 
-    
-    public static SingletoneEscaner getInstance() 
-    { 
-        if (single_instance == null) 
-            single_instance = new SingletoneEscaner(); 
-  
-        return single_instance; 
-    } 
-    
-    
-    public String usarJflex(Lexer lexer) throws IOException{
-        String Resultados = "";
-        LinkedHashMap<String, LinkedHashMap<String,Integer>> palabras = new LinkedHashMap<>();
-        
-        while (true) {
-            Token token = lexer.yylex();
-            if (token == null) {
-                break;
-                //Resultados = Resultados + "FIN";
-                //return Resultados;
-            }//termina evaluacion
-            switch (token) {
-                
-                case IDENTIFICADOR: case OPERADOR: case PALABRA_RESERVADA: case TRANSAC: case UNIDAD: case LITERAL:
-                    System.out.println(lexer.lexeme);
-                    Integer ultimoEspacio= lexer.lexeme.lastIndexOf(' ');
-                    String stringToken=token+" "+lexer.lexeme.substring(0, ultimoEspacio);
-                    String numeroLinea =lexer.lexeme.substring(ultimoEspacio+1);
-                    if (palabras.get(stringToken) == null){
-                        LinkedHashMap<String,Integer> lineas = new LinkedHashMap<>();                        
-                        lineas.put(numeroLinea,1);
-                        palabras.put(stringToken, lineas);
-                    }else{
-                        palabras.get(stringToken).put(numeroLinea,palabras.get(stringToken).getOrDefault(numeroLinea,0)+1);
-                    }  
-                    System.out.println(palabras); 
-                    System.out.println(token+" "+lexer.lexeme);
-                    //Resultados = Resultados + "Token:" + token + " " + lexer.lexeme + "\n";
-                break;
-                case ERROR:
-                     ultimoEspacio= lexer.lexeme.lastIndexOf(' ');
-                    stringToken=token+" "+lexer.lexeme.substring(0, ultimoEspacio);
-                    numeroLinea =lexer.lexeme.substring(ultimoEspacio+1);
-                    if (palabras.get(stringToken) == null){
-                        LinkedHashMap<String,Integer> lineas = new LinkedHashMap<>();                        
-                        lineas.put(numeroLinea,1);
-                        palabras.put(stringToken, lineas);
-                    }else{
-                        palabras.get(stringToken).put(numeroLinea,palabras.get(stringToken).getOrDefault(numeroLinea,0)+1);
-                    }  
-                    System.out.println(palabras); 
-                    System.out.println(token+" "+lexer.lexeme);
-                    //Resultados = Resultados + "Token:" + token + " " + lexer.lexeme + "\n";                    
-            }
-        }
-        for (String palabra : palabras.keySet()){
-           Resultados = Resultados + palabra; 
-           for (String linea : palabras.get(palabra).keySet()){
-              Resultados = Resultados + " " +linea + "("+palabras.get(palabra).get(linea) +"),"; 
-           }
-           Resultados=Resultados.substring(0, Resultados.length() - 1)+ "\n";
-        }
-        Resultados = Resultados +"FIN";
-        return Resultados;
+
+  private static SingletoneEscaner single_instance = null;
+
+  private SingletoneEscaner() {
+  }
+
+  public static SingletoneEscaner getInstance() {
+    if (single_instance == null) {
+      single_instance = new SingletoneEscaner();
     }
+
+    return single_instance;
+  }
+
+  public LinkedHashMap<String, LinkedHashMap<String, Integer>> usarJflex(Lexer lexer) throws IOException {
+    String Resultados = "";
+    LinkedHashMap<String, LinkedHashMap<String, Integer>> palabras = new LinkedHashMap<>();
+
+    while (true) {
+      Token token = lexer.yylex();
+      if (token == null) {
+        break;
+        //Resultados = Resultados + "FIN";
+        //return Resultados;
+      }//termina evaluacion
+      switch (token) {
+
+        case IDENTIFICADOR:
+        case OPERADOR:
+        case PALABRA_RESERVADA:
+        case TRANSAC:
+        case UNIDAD:
+        case LITERAL:
+          System.out.println(lexer.lexeme);
+          Integer ultimoEspacio = lexer.lexeme.lastIndexOf(' ');
+          String stringToken = token + " " + lexer.lexeme.substring(0, ultimoEspacio);
+          String numeroLinea = lexer.lexeme.substring(ultimoEspacio + 1);
+          if (palabras.get(stringToken) == null) {
+            LinkedHashMap<String, Integer> lineas = new LinkedHashMap<>();
+            lineas.put(numeroLinea, 1);
+            palabras.put(stringToken, lineas);
+          } else {
+            palabras.get(stringToken).put(numeroLinea, palabras.get(stringToken).getOrDefault(numeroLinea, 0) + 1);
+          }
+          System.out.println(palabras);
+          System.out.println(token + " " + lexer.lexeme);
+          //Resultados = Resultados + "Token:" + token + " " + lexer.lexeme + "\n";
+          break;
+        case ERROR:
+          ultimoEspacio = lexer.lexeme.lastIndexOf(' ');
+          stringToken = token + " " + lexer.lexeme.substring(0, ultimoEspacio);
+          numeroLinea = lexer.lexeme.substring(ultimoEspacio + 1);
+          if (palabras.get(stringToken) == null) {
+            LinkedHashMap<String, Integer> lineas = new LinkedHashMap<>();
+            lineas.put(numeroLinea, 1);
+            palabras.put(stringToken, lineas);
+          } else {
+            palabras.get(stringToken).put(numeroLinea, palabras.get(stringToken).getOrDefault(numeroLinea, 0) + 1);
+          }
+          System.out.println(palabras);
+          System.out.println(token + " " + lexer.lexeme);
+        //Resultados = Resultados + "Token:" + token + " " + lexer.lexeme + "\n";                    
+      }
+    }
+    for (String palabra : palabras.keySet()) {
+      Resultados = Resultados + palabra;
+      for (String linea : palabras.get(palabra).keySet()) {
+        Resultados = Resultados + " " + linea + "(" + palabras.get(palabra).get(linea) + "),";
+      }
+      Resultados = Resultados.substring(0, Resultados.length() - 1) + "\n";
+    }
+    Resultados = Resultados + "FIN";
+    return palabras;
+  }
 }
