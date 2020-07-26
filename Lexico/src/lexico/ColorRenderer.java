d05c91ea7534330012fd1d3ed2841b532f6e351c
FelipePacheco <felipepace09@gmail.com>
2020-07-25 23:32:06 -0600

    uwu

--- Lexico/src/lexico/ColorRenderer.java
+++ Lexico/src/lexico/ColorRenderer.java
@@ -26,6 +26,9 @@ class ColorRenderer extends DefaultTableCellRenderer {
       String s = ((String) value);
       value = "<html><font color=white> " + s.substring(0, s.indexOf('~')) + "</font> <font color=red>" + s.substring(s.indexOf('~') + 1, s.lastIndexOf('~')) + "</font> <font color=white>" + " ..." + "</font> </html>";
     }
+    if (((String) value).equals("~EOF~")) {
+      value = "<html><font color=red>" + "EOF" + "</font> </html>";
+    }
     Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
 //"<html><font size='5' color=blue> Welcome to</font> <font            size='6'color=green> Tutorials Point</font></html>"
